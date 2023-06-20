2f44e8d05b0a1ae544e91c493cfebeea04211661
Lahiru Jayaratne <lahiruj5@gmail.com>
2023-06-20 23:33:08 +0530

    Add show action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,16 @@
 class RoomsController < ApplicationController
+  before_action :find_room, only: %i[show]
 
   def index
     @rooms = Room.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_room
+    @room = Room.find(params[:id])
   end
 end
