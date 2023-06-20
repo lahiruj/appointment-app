ded1a65388810af9bf14c91c593eb77503a958f1
Lahiru Jayaratne <lahiruj5@gmail.com>
2023-06-20 23:57:12 +0530

    Add form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :find_room, only: %i[show]
+  before_action :find_room, only: %i[show edit update destroy]
 
   def index
     @rooms = Room.all
@@ -8,7 +8,41 @@ class RoomsController < ApplicationController
   def show
   end
 
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to rooms_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to rooms_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @room.destory
+    redirect_to rooms_path
+  end
+
   private
+
+  def room_params
+    params.require(:room).permit(:name, :capacity)
+  end
 
   def find_room
     @room = Room.find(params[:id])
